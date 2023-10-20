0b589d3d3141f31b4d29d032cd58770401cf7701
KajetanKalinowski <kajtul999@gmail.com>
2023-10-20 14:18:53 +0200

    part2

--- app.js
+++ app.js
@@ -14,33 +14,53 @@ async function getItems(){
     div.classList.add("divs")
     const h1 = document.createElement("h1")
     const h2 = document.createElement("h2")
+    const h3 = document.createElement("h3")
+    const h4 = document.createElement("h4")
     h1.innerHTML=json[i].name
     h2.innerHTML=json[i].price+" zł"
+    h3.innerHTML="Sprzedano "+json[i].total_sales+" razy!"
     const miejsce = document.createElement("div")
     miejsce.id="wow"
     const wzrost = document.createElement("button")
     wzrost.innerHTML="+10zł"
     const minus = document.createElement("button")
     minus.innerHTML="-10zł"
+    const zatw = document.createElement("button")
+    zatw.innerHTML="ZATWIERDZ"
+    
 
     wzrost.addEventListener('click',()=>{
-        wzrostCen(json[i].id,json[i].regular_price)
+        h4.innerHTML="Czy zwiększyć cenę o 10 zł?"
+        zatw.addEventListener('click',()=>{
+            wzrostCen(json[i].id,json[i].regular_price)        
+        })
+        
     })
     if(json[i].price>10){
     minus.addEventListener('click',()=>{
+        h4.innerHTML="Czy zmiejszyć cenę o 10 zł?"
+        zatw.addEventListener('click',()=>{
         minusCen(json[i].id,json[i].regular_price)
+        })
     })
 }else{
     
     minus.style.color = "red"
     minus.style.backgroundColor = "rgb(39, 39, 39)"
 }
+zatw.addEventListener('click',()=>{
+
+        
+})
 
     div.appendChild(h1)
     div.appendChild(h2)
+    div.appendChild(h3)
+    div.appendChild(h4)
     div.appendChild(miejsce)
     div.appendChild(wzrost)
     div.appendChild(minus)
+    div.appendChild(zatw)
     body.appendChild(div)
 
 
